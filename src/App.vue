5f436be1fea52b6231f76101473ce8e1c915db35
flutterbob <haibudongshi@gmail.com>
2020-07-20 02:03:50 +0800

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,64 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <a-layout id="components-layout-demo-fixed">
+      <a-layout-header>
+        <div class="logo" />
+        <a-menu theme="dark" mode="horizontal" :default-selected-keys="['2']">
+          <a-menu-item key="1">nav 1</a-menu-item>
+          <a-menu-item key="2">nav 2</a-menu-item>
+          <a-menu-item key="3">nav 3</a-menu-item>
+        </a-menu>
+      </a-layout-header>
+      <a-layout-content>
+        <a-breadcrumb>
+          <a-breadcrumb-item>Home</a-breadcrumb-item>
+          <a-breadcrumb-item>List</a-breadcrumb-item>
+          <a-breadcrumb-item>App</a-breadcrumb-item>
+        </a-breadcrumb>
+        <div id="content">Content</div>
+      </a-layout-content>
+      <a-layout-footer>Ant Design ©2020 Created by Yi Zheng</a-layout-footer>
+    </a-layout>
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<style lang="less" scope>
+#components-layout-demo-fixed .logo {
+  width: 120px;
+  height: 31px;
+  background: rgba(255, 255, 255);
+  margin: 16px 24px 16px 0;
+  float: left;
 }
 
-#nav {
-  padding: 30px;
+#content {
+  background: #fff;
+  padding: 24px;
+  min-height: 380px;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.ant-layout-header {
+  position: fixed;
+  z-index: 1;
+  width: 100%;
+  // background-color: rgba(255, 255, 255, 0.2);
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.ant-layout-content {
+  padding: 0 50px;
+  margin-top: 64px;
+}
+
+.ant-menu {
+  line-height: 64px;
+  float: right;
+}
+
+.ant-layout-footer {
+  text-align: center;
+}
+
+.ant-breadcrumb {
+  margin: 16px 0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// import Button from 'ant-design-vue/lib/button'
+// import 'ant-design-vue/dist/antd.css'
+import { Layout, Breadcrumb, Menu } from 'ant-design-vue'
+Vue.use(Layout)
+Vue.use(Menu)
+Vue.use(Breadcrumb)
+
 Vue.config.productionTip = false
 
 new Vue({
